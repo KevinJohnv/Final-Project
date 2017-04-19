d4d12750478e6c59523dbe9f00097d03d11b3dfa
kyizman <kyson.manis@yahoo.com>
2017-04-18 21:29:54 -0400

    Added to PishtiPane

--- src/Computer.java
+++ src/Computer.java
@@ -13,4 +13,22 @@ public class Computer extends Player {
     public Integer size(){
         return compDeck.size();
     }
+
+    public void computerplay(ArrayList<Integer> table)
+    {
+        boolean hasmatch=false;
+        for(int x = 0;x<compDeck.size();x++) {
+            if (compDeck.get(x)%13 == table.get(table.size()-1)%13) {
+                playCard(compDeck.get(x));
+                hasmatch = true;
+                break;
+            }
+
+        }
+        if(hasmatch==false)
+        {
+            playCard(compDeck.get(compDeck.get(compDeck.size()-1)));
+        }
+    }
 }
+// yes this is a test 
